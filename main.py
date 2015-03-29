3b0324ee4cfe82b8f3ad4345dd9eb12a873e72d7
j <moraaljj@gmail.com>
2015-03-29 18:14:44 -0400

    First draft done?

--- main.py
+++ main.py
@@ -1,29 +1,24 @@
-# Joe Moraal and Braedon McCoy
+## Braedon McCoy and Joseph Moraal ##
 
+# Note enum requires python version 3.0 or greater
 from enum import Enum
 import random
-
 
 def evalHand(self, hand):
     retval = 0
     left, middle, right = hand
 
-    if left > middle then ++retval
-    if middle > right then ++retval
-    if left > right then ++retval
+    if left > middle:
+        ++retval
+    if middle > right:
+        ++retval
+    if left > right:
+        ++retval
 
-    return retval
-
-class Player:
-    def __init__(self, name, hand):
-        self.hand = hand
-        self.name = name
-
-    def complete(self):
-        return abs(evalHand(self.hand)) == 3
+    return float(retval)
 
 ## version of deal with randomization.
-def deprecated_deal():
+def randDeal():
     cardList = [x for x in range(1, 8)]
     print(cardList)
     cards = []
@@ -32,14 +27,14 @@ def deprecated_deal():
         index = random.randint(0, len(cardList) - 1)
         cards.append(cardList.pop(index))
 
-    playerMax = Player("max", (cards[0], cards[2], cards[4]))
-    playerMin = Player("min", (cards[1], cards[3], cards[5]))
-    return (playerMax, playerMin, cards[6])
+    maxHand = (cards[0], cards[2], cards[4])
+    minHand = (cards[1], cards[3], cards[5])
+    return (maxHand, minHand, cards[6])
 
 def deal():
-    playerMax = Player("max", (6, 3, 1))
-    playerMin = Player("min", (7, 5, 2))
-    return (playerMax, playerMin, 4)
+    maxHand = (6, 3, 1)
+    minHand = (7, 5, 2)
+    return (maxHand, minHand, 4)
 
 class Node:
     def __init__(self, maxHand, minHand, middleCard):
@@ -50,22 +45,22 @@ class Node:
 
 def genNodes_recursiveHelper(node, isMax, depth):
     if depth < 3:
-        left, middle, right = (if isMax then node.maxHand else node.minHand)
+        left, middle, right = (node.maxHand if isMax else node.minHand)
 
         leftSwap = (node.middleCard, middle, right)
         middleSwap = (left, node.middleCard, right)
         rightSwap = (left, middle, node.middleCard)
 
-        maxHand = (if isMax then leftSwap else node.maxHand)
-        minHand = (if isMax then node.minHand else leftSwap)
+        maxHand = (leftSwap if isMax else node.maxHand)
+        minHand = (node.minHand if isMax else leftSwap)
         leftChild = Node(maxHand, minHand, left)
 
-        maxHand = (if isMax then middleSwap else node.maxHand)
-        minHand = (if isMax then node.minHand else middleSwap)
+        maxHand = (middleSwap if isMax else node.maxHand)
+        minHand = (node.minHand if isMax else middleSwap)
         middleChild = Node(maxHand, minHand, middle)
 
-        maxHand = (if isMax then rightSwap else node.maxHand)
-        minHand = (if isMax then node.minHand else rightSwap)
+        maxHand = (rightSwap if isMax else node.maxHand)
+        minHand = (node.minHand if isMax else rightSwap)
         rightChild = Node(maxHand, minHand, right)
 
         leftChild = genNodes_recursiveHelper(leftChild, not isMax, depth + 1)
@@ -80,24 +75,50 @@ def genNodes(playerMax, playerMin, middleCard):
     rootNode = Node(playerMax.hand, playerMin.hand, middleCard)
     return genNodes_recursiveHelper(rootNode, True, 0)
 
-def pruneNodes_recursiveHelper():
-    pass
+def printLeaf(node, h, isPruned):
+    prunedString = ("true" if isPruned else "false")
+    minLeft, minMiddle, minRight = node.minHand
+    maxLeft, maxMiddle, maxRight = node.maxHand
+    values = (minLeft, minMiddle, minRight, node.middleCard, maxLeft, maxMiddle, maxRight, h, prunedString)
+    print("(min = {%d, %d, %d}) (middle = %d) (max = {%d, %d, %d}) -> (h = %d) (pruned = %s") % values)
 
-def pruneNodes():
-    pass
+def shouldPrune(h, parentH, isMax):
+    if isMax and (h > parentH):
+        return True
+    elif not isMax and (h < parentH):
+        return True
 
-def evalMove(hand, middleCard):
-    left, middle, right = hand
+    return False
 
-    hl = evalHand((middleCard, middle, right))
-    hm = evalHand((left, middleCard, right))
-    hr = evalHand((left, middle, middleCard))
+def pruneNodes_recursiveHelper(node, isMax, parentH, isPruned):
+    h = eval(node.maxHand if isMax else node.minHand)
 
-    return (hl, hm, hr)
+    if node.children:
+        left, middle, right = node.children
+
+        h = pruneNodes_recursiveHelper(left, not isMax, parentH, isPruned)
+        pruneRemaining = shouldPrune(h, parentH, isMax)
+
+        middleH = pruneNodes_recursiveHelper(middle, not isMax, h, pruneRemaining)
+        if not pruneRemaining and ((isMax and h < middleH) or (not isMax and h > middleH)):
+            h = middleH
+            pruneRemaining = shouldPrune(h, parentH, isMax)
+
+        rightH = pruneNodes_recursiveHelper(right, not isMax, h, pruneRemaining)
+        if not pruneRemaining and ((isMax and h < rightH) or (not isMax and h > rightH)):
+            h = rightH
+    else:
+        printLeaf(node, h, isPruned)
+
+    return h
+
+def pruneNodes(rootNode):
+    result = pruneNodes_recursiveHelper(rootNode, False, float("Inf"))
+    return result.h
 
 if __name__ == "__main__":
-    playerMax, playerMin, middleCard = deal()
-    rootNode = genNodes(playerMax, playerMin, middleCard)
-    result = pruneNodes(rootNode)
+    maxHand, minHand, middleCard = deal()
+    rootNode = genNodes(maxHand, minHand, middleCard)
+    h = pruneNodes(rootNode)
 
-    print ("Result: %d" % (result))
+    print ("Result: %d" % (h))
